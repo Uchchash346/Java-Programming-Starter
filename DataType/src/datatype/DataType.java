0d5c8d344d08953764de4dc4ef1704da2bb7e0a5
Uchchash346 <mru416@gmail.com>
2022-04-06 17:12:40 +0600

    Format Specifier

--- DataType/src/datatype/DataType.java
+++ DataType/src/datatype/DataType.java
@@ -4,19 +4,24 @@ public class DataType {
 
     public static void main(String[] args) {
         boolean b = true; // dynamic initialization 
-        char c;
+        char c = 'a';
         short s = 32677;
         int i = 11276;
         float f = 10.2f;
         double d = 10.2;
 
-        System.out.println("b = " + b);
-        c = 'a';
-        System.out.println("c = " + c);
-        System.out.println("s = " + s);
-        System.out.println("i = " + i);
-        System.out.println("f = " + f);
-        System.out.println("d = " + d);
+//        System.out.println("b = " + b);
+//        c = 'a';
+//        System.out.println("c = " + c);
+//        System.out.println("s = " + s);
+//        System.out.println("i = " + i);
+//        System.out.println("f = " + f);
+//        System.out.println("d = " + d);
+        System.out.printf("boolean b = %b\n", b);
+        System.out.printf("Character c = %c\n", c);
+        System.out.printf("Short s = %d\n", s);
+        System.out.printf("Double d = %.3f\n", d);
+
     }
 
 }
