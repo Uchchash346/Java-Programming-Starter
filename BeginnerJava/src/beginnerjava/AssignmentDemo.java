5824c65e7299ad2066ef6e3ed2d744bb20826f64
Uchchash346 <mru416@gmail.com>
2022-04-07 00:52:55 +0600

    Assignment Operator

---
+++ BeginnerJava/src/beginnerjava/AssignmentDemo.java
@@ -0,0 +1,24 @@
+package beginnerjava;
+
+public class AssignmentDemo {
+
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 2;
+
+        x += y;
+        System.out.println("x = " + x);
+
+        x -= y;
+        System.out.println("x = " + x);
+
+        x *= y;
+        System.out.println("x = " + x);
+
+        x /= y;
+        System.out.println("x = " + x);
+
+        x %= y;
+        System.out.println("x = " + x);
+    }
+}
