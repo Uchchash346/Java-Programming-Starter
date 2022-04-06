7e7960f28717257a187c4372b7d4835dc743b411
Uchchash346 <mru416@gmail.com>
2022-04-06 15:55:12 +0600

    Variables and data types

---
+++ DataType/src/datatype/DataType.java
@@ -0,0 +1,22 @@
+package datatype;
+
+public class DataType {
+
+    public static void main(String[] args) {
+        boolean b = true; // dynamic initialization 
+        char c;
+        short s = 32677;
+        int i = 11276;
+        float f = 10.2f;
+        double d = 10.2;
+
+        System.out.println("b = " + b);
+        c = 'a';
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+    }
+
+}
