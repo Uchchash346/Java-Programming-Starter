0886bdf01b9c245fb581e0b41504379690761a8d
Uchchash346 <mru416@gmail.com>
2022-04-06 14:52:36 +0600

    First Java application

---
+++ BasicJava/src/basicjava/BasicJava.java
@@ -0,0 +1,9 @@
+
+package basicjava;
+public class BasicJava {
+    public static void main(String[] args) {
+        System.out.println("Mahmudur Rahman");
+        System.out.println("01875311111");
+    }
+    
+}
