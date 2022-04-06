05f1698e607b38f0bbac2587fe7f1bf539e2c1f9
Uchchash346 <mru416@gmail.com>
2022-04-06 15:04:27 +0600

    Escape sequence

--- BasicJava/src/basicjava/BasicJava.java
+++ BasicJava/src/basicjava/BasicJava.java
@@ -1,9 +1,13 @@
-
 package basicjava;
+
 public class BasicJava {
+
     public static void main(String[] args) {
-        System.out.println("Mahmudur Rahman");
-        System.out.println("01875311111");
+        System.out.println("Mahmudur Rahman \nI am from Dhaka, Bangladesh");
+        System.out.println("\"Bangladesh\"");
+        System.out.println("1 \t 2");
+        System.out.println("3 \t 4");
+//        System.out.println("01875311111");
     }
-    
+
 }
