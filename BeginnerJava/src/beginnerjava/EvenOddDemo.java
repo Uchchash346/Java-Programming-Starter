1690f82120ffa854998c0cedbcee40e95cc6ff61
Uchchash346 <mru416@gmail.com>
2022-04-08 21:17:47 +0600

    Even odd

---
+++ BeginnerJava/src/beginnerjava/EvenOddDemo.java
@@ -0,0 +1,19 @@
+package beginnerjava;
+
+import java.util.Scanner;
+
+public class EvenOddDemo {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num;
+        System.out.println("Enter any positive number: ");
+        num = input.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+}
