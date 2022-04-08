566a23613a977313ac67ee72b3cc20f487e66ed5
Uchchash346 <mru416@gmail.com>
2022-04-08 20:03:29 +0600

    Temperature Converter

---
+++ BeginnerJava/src/beginnerjava/TemperatureDemo.java
@@ -0,0 +1,17 @@
+package beginnerjava;
+
+import java.util.Scanner;
+
+public class TemperatureDemo {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double cels, farn;
+
+        System.out.print("Celsius = ");;
+        cels = input.nextDouble();
+
+        farn = 1.8 * cels + 32;
+        System.out.println("Fahrenheit: " + farn);
+    }
+}
