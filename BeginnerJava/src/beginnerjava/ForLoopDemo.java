b1a07e321e41b77701fbf16b5fdb461b66130b41
Uchchash346 <mru416@gmail.com>
2022-04-10 03:11:30 +0600

    For Loop

---
+++ BeginnerJava/src/beginnerjava/ForLoopDemo.java
@@ -0,0 +1,10 @@
+package beginnerjava;
+
+public class ForLoopDemo {
+
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
