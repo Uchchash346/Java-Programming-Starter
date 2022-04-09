f462f2b71bd2d93df8ad396c0510f4f1cfc2cd55
Uchchash346 <mru416@gmail.com>
2022-04-10 02:15:17 +0600

    Switch statement

---
+++ BeginnerJava/src/beginnerjava/DigitSpelling.java
@@ -0,0 +1,27 @@
+package beginnerjava;
+
+import java.util.Scanner;
+
+public class DigitSpelling {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int digit;
+        System.out.println("Enter any Digit: ");
+        digit = input.nextInt();
+
+        switch (digit) {
+            case 0 -> System.out.println("Zero");
+            case 1 -> System.out.println("One");
+            case 2 -> System.out.println("Two");
+            case 3 -> System.out.println("Three");
+            case 4 -> System.out.println("Four");
+            case 5 -> System.out.println("Five");
+            case 6 -> System.out.println("Six");
+            case 7 -> System.out.println("Seven");
+            case 8 -> System.out.println("Eight");
+            case 9 -> System.out.println("Nine");
+            default -> System.out.println("Not a digit");
+        }
+    }
+}
