231ff08fe504543cbd8d255b1729c80fbfa74eeb
Uchchash346 <mru416@gmail.com>
2022-04-11 05:47:41 +0600

    Do While Loop vs while

--- BeginnerJava/src/beginnerjava/DoWhileLoopDemo.java
+++ BeginnerJava/src/beginnerjava/DoWhileLoopDemo.java
@@ -3,11 +3,10 @@ package beginnerjava;
 public class DoWhileLoopDemo {
 
     public static void main(String[] args) {
-        int i = 2;
+        int x = 6;
         do {
-            System.out.println(i);
-            i = i + 2;
-        } while (i <= 100);
+            System.out.println("Bangladesh");
+        } while (x <= 5);
     }
 
 }
