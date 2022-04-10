793ba43a17ad03c5a7569eae1ca6bc5dcaf2eb64
Uchchash346 <mru416@gmail.com>
2022-04-11 05:56:54 +0600

    Break and Continue

--- BeginnerJava/src/beginnerjava/ContinueDemo.java
+++ BeginnerJava/src/beginnerjava/ContinueDemo.java
@@ -3,7 +3,7 @@ package beginnerjava;
 public class ContinueDemo {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i = i + 3) {
+        for (int i = 1; i <= 100; i++) {
             if (i == 10) {
                 continue;
             }
