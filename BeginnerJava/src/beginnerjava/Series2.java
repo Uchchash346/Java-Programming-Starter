4c2c15f122503765203fa98c8709f6ee75b4f4d8
Uchchash346 <mru416@gmail.com>
2022-04-11 06:36:56 +0600

    Series

---
+++ BeginnerJava/src/beginnerjava/Series2.java
@@ -0,0 +1,20 @@
+package beginnerjava;
+
+import java.util.Scanner;
+
+public class Series2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, result = 1;
+        System.out.print("Enter the last number: ");
+        n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+            result = result * i;
+        }
+        System.out.println();
+        System.out.println(result);
+    }
+}
